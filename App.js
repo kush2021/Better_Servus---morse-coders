fff11b329990e8167a1f9f93d49d030e38d4c31b
kush2021 <kushpadalia2002@gmail.com>
2023-01-07 13:52:37 -0700

    Added icons to screens in the navigation bar.

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
-import Footer from "./Components/Footer";
 import Accounts from "./Screens/Accounts";
 import More from "./Screens/More";
 import MoveMoney from "./Screens/MoveMoney";
+import { Icon } from "react-native-elements";
+import { StyleSheet } from "react-native";
 
 const Drawer = createDrawerNavigator();
 
@@ -12,14 +13,61 @@ export default function App() {
     return (
         <NavigationContainer>
             <Drawer.Navigator
-                screenOptions={{ headerStyle: { backgroundColor: "#3070B6" },
-								headerTitleStyle: {color: "#fff"},
-                                headerTintColor: "#fff"
-                                }}>
-                <Drawer.Screen name="Accounts" component={Accounts} />
-                <Drawer.Screen name="Move Money" component={MoveMoney} />
-                <Drawer.Screen name="More" component={More} />
+                screenOptions={{
+                    headerStyle: { backgroundColor: "#3070B6" },
+                    headerTitleStyle: { color: "#fff" },
+                    headerTintColor: "#fff",
+                }}
+            >
+                <Drawer.Screen
+                    name="Accounts"
+                    component={Accounts}
+                    options={{
+                        drawerIcon: () => (
+                            <Icon
+                                name="dollar-sign"
+                                type="feather"
+                                color="#ABAFBA"
+                                size="15"
+                                style={styles.icon}
+                            />
+                        ),
+                    }}
+                />
+                <Drawer.Screen name="Move Money" 
+                component={MoveMoney} 
+                options={{
+                    drawerIcon: () => (
+                        <Icon
+                            name="refresh-cw"
+                            type="feather"
+                            color="#ABAFBA"
+                            size="15"
+                            style={styles.icon}
+                        />
+                    )
+                }}/>
+                <Drawer.Screen name="More"
+                component={More} 
+                options={{
+                    drawerIcon: () => (
+                        <Icon
+                            name="plus"
+                            type="feather"
+                            color="#ABAFBA"
+                            size="15"
+                            style={styles.icon}
+                        />
+                    )
+                }}/>
             </Drawer.Navigator>
         </NavigationContainer>
     );
 }
+
+const styles = StyleSheet.create({
+    icon: {
+        paddingRight: 0,
+        width: 15
+    }
+});
