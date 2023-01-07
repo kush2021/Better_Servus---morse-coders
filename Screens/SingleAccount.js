1bd229419fb245ccf292fd73ead6ae2e9ab9fc0c
Jack Parsons <jackpar1113@gmail.com>
2023-01-07 13:19:39 -0700

    implement accounts screen UI

---
+++ Screens/SingleAccount.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function SingleAccount() {
+  return (
+    <View>
+      <Text>SingleAccount</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
