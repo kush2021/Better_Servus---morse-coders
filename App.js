af2fed4ce64558cabe2b70a1179de587d9038379
Grace Mullane <68264537+gracemullane@users.noreply.github.com>
2023-01-07 14:43:19 -0700

    Testing it out

--- App.js
+++ App.js
@@ -4,10 +4,12 @@ import Footer from './Components/Footer';
 import Accounts from './Screens/Accounts';
 import More from './Screens/More';
 import MoveMoney from './Screens/MoveMoney';
+import Login from './Screens/Login';
 
-const Stack = createNativeStackNavigator();
+const AuthContext = React.createContext();
 
-export default function App() {
+function HomeScreen() {
+  const { signOut } = React.useContext(AuthContext);
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -30,4 +32,103 @@ export default function App() {
       <Footer />
     </NavigationContainer>
   );
+}
+
+function SignInScreen() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const { signIn } = React.useContext(AuthContext);
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+          <Stack.Screen
+            name="Sign In"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  const [state, dispatch] = React.useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          };
+      }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
+    }
+  );
+
+  React.useEffect(() => {
+    // Fetch the token from storage then navigate to our appropriate place
+    const bootstrapAsync = async () => {
+      let userToken;
+
+      try {
+        // Restore token stored in `SecureStore` or any other encrypted storage
+        // userToken = await SecureStore.getItemAsync('userToken');
+      } catch (e) {
+        // Restoring token failed
+      }
+
+      // After restoring token, we may need to validate it in production apps
+
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+    };
+
+    bootstrapAsync();
+  }, []);
+
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async (data) => {
+        // In a production app, we need to send some data (usually username, password) to server and get a token
+        // We will also need to handle errors if sign in failed
+        // After getting token, we need to persist the token using `SecureStore` or any other encrypted storage
+        // In the example, we'll use a dummy token
+
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+      signOut: () => dispatch({ type: 'SIGN_OUT' }),
+      signUp: async (data) => {
+        // In a production app, we need to send user data to server and get a token
+        // We will also need to handle errors if sign up failed
+        // After getting token, we need to persist the token using `SecureStore` or any other encrypted storage
+        // In the example, we'll use a dummy token
+
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+    }),
+    []
+  );
+
 }
---
+++ Screens/Login.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { Alert, Button, Text, TouchableOpacity, TextInput, View, StyleSheet } from 'react-native';
+
+export default class App extends Component {
+
+    state = {
+      email: '',
+      password: '',
+    };
+  
+  
+  onLogin() {
+    const { email, password } = this.state;
+
+    Alert.alert('Credentials', `email: ${email} + password: ${password}`);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <Text style={styles.titleText}>Welcome</Text>
+        <Text style={styles.titleText}>Login</Text>
+        <TextInput
+          value={this.state.email}
+          keyboardType = 'email-address'
+          onChangeText={(email) => this.setState({ email })}
+          placeholder='email'
+          placeholderTextColor = 'white'
+          style={styles.input}
+        />
+        <TextInput
+          value={this.state.password}
+          onChangeText={(password) => this.setState({ password })}
+          placeholder={'password'}
+          secureTextEntry={true}
+          placeholderTextColor = 'white'
+          style={styles.input}
+        />
+        
+     
+        <TouchableOpacity
+          style={styles.button}
+          onPress={this.onLogin.bind(this)}
+       >
+         <Text style={styles.buttonText}> Sign Up / Login </Text>
+       </TouchableOpacity>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#3070B6',
+  },
+  titleText:{
+    fontFamily: 'Baskerville',
+    fontSize: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: 'powderblue',
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+    borderRadius: 25,
+    marginBottom: 10,
+  },
+  buttonText:{
+    fontFamily: 'Baskerville',
+    fontSize: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    width: 200,
+    fontFamily: 'Baskerville',
+    fontSize: 20,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+    marginVertical: 10,
+  },
+});
